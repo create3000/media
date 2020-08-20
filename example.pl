bdc7ddbd88add9f766bd4dbbdb33e17268335e87
Holger Seelig <holger.seelig@gmail.com>
2020-08-20 09:44:01 +0200

    Updated examples.html files.

--- example.pl
+++ example.pl
@@ -10,7 +10,7 @@ sub example {
     $file = `cat '$filename'`;
 
     $file =~ s|http://code\.create3000\.de/x_ite/latest/dist/x_ite\.css|https://create3000.github.io/code/x_ite/latest/dist/x_ite.css|sgo;
-    $file =~ s|http://code\.create3000\.de/x_ite/latest/dist/x_ite\.min/.js|https://create3000.github.io/code/x_ite/latest/dist/x_ite.min.js|sgo;
+    $file =~ s|http://code\.create3000\.de/x_ite/latest/dist/x_ite\.min\.js|https://create3000.github.io/code/x_ite/latest/dist/x_ite.min.js|sgo;
     $file =~ s|http://create3000\.de/x_ite/|https://github.com/create3000/x_ite/wiki|sgo;
     $file =~ s| target="_blank"||sgo;
 
