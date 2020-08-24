a577dbe26286244600df569f1f73933a612a0973
Holger Seelig <holger.seelig@gmail.com>
2020-08-24 14:41:28 +0200

    Updated tutorials.

--- example.pl
+++ example.pl
@@ -4,8 +4,8 @@ use v5.10.0;
 use open qw/:std :utf8/;
 
 use File::Basename qw(basename dirname);
+use Cwd;
 
-print "hello\n";
 system "pwd";
 
 sub example {
@@ -13,16 +13,12 @@ sub example {
     print $filename;
     chomp $filename;
 
+    say $filename;
+    return;
+
     # example.html
 
     $file = `cat '$filename'`;
-
-    $file =~ s|http://code\.create3000\.de/x_ite/latest/dist/x_ite\.css|https://create3000.github.io/code/x_ite/latest/dist/x_ite.css|sgo;
-    $file =~ s|http://code\.create3000\.de/x_ite/latest/dist/x_ite\.min\.js|https://create3000.github.io/code/x_ite/latest/dist/x_ite.min.js|sgo;
-    $file =~ s|http://create3000\.de/x_ite/|https://github.com/create3000/x_ite/wiki|sgo;
-    $file =~ s| target="_blank"||sgo;
-    $file =~ s|class="download-button" href="(.*?)\.x3d"|class="download-button" href="$1.zip"|sgo;
-    $file =~ s|getMetadata|getMetaDatas|sgo;
 
     $filename =~ s|$o|$n|;
 
@@ -41,9 +37,7 @@ sub example {
     system "mv '$d.zip' '$d/'";
 }
 
-$o = "/Volumes/Home/Projekte/create3000.de/media/htdocs/components/";
-$n = "/Volumes/Home/Projekte/media/docs/examples/";
+$docs = "/Volumes/Home/Projekte/media/docs/";
+$cwd  = cwd ();
 
-$cwd = `pwd`;
-chomp $cwd;
-example $_ foreach `find $o -type f -name 'example.html'`;
+example $_ foreach `find $docs -type f -name 'example.html'`;
