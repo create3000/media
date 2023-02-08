094a8bf1c17bae9ac3912721f1b7d5156ed5d15f
Holger Seelig <holger.seelig@gmail.com>
2023-02-08 14:31:54 +0100

    Added debug.

---
+++ build/debug.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+npx ../x3d-tidy -r -i '/Users/holger/Desktop/X_ITE/media/docs/examples/Picking/LinePickSensor/LinePickSensor.O.x3d' -o '/Users/holger/Desktop/X_ITE/media/docs/examples/Picking/LinePickSensor/LinePickSensor.x3d'
