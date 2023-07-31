f3e58f60071e6efad5f9b99c00538c0c8fc06f02
Holger Seelig <holger.seelig@gmail.com>
2023-07-31 20:39:46 +0200

    Updated screenshots.

--- build/images.pl
+++ build/images.pl
@@ -7,6 +7,7 @@ use open qw/:std :utf8/;
 use Cwd;
 use File::Basename qw(basename dirname);
 use List::MoreUtils qw(any);
+use Getopt::Long;
 
 $cwd      = cwd ();
 $examples = "$cwd/docs/examples";
@@ -20,9 +21,14 @@ sub image {
    say $base;
 
    chdir $folder;
-   system "npx", "--yes", "x3d-image", "-s", "1000x562", "-i", "$base.O.x3d", "-o", "screenshot.png";
-   system "npx", "--yes", "x3d-image", "-s", "110x62",   "-i", "$base.O.x3d", "-o", "screenshot-small.png";
+   system "npx", "--yes", "x3d-image", @extra, "-s", "1000x562", "-i", "$base.x3d", "-o", "screenshot.png";
+   system "convert", "-resize", "110x62", "screenshot.png", "screenshot-small.png";
 }
+
+@extra = ();
+$delay = 0;
+GetOptions ("delay=i" => \$delay);
+push @extra, "-d", $delay;
 
 @files = sort `find $examples -maxdepth 2 -mindepth 2 -type d`;
 @files = grep { $s = $_; any { index ($s, $_) != -1 } @ARGV } @files if @ARGV;
