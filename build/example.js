06318a19d3e02362bfeacdf57a5f9e4ad9e8bb3c
Holger Seelig <holger.seelig@gmail.com>
2024-03-10 20:14:56 +0100

    Updated Fog and LocalFog.

---
+++ build/example.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+"use strict";
+
+const { systemSync } = require ("shell-tools");
+
+function main ()
+{
+   const argv = new Set (process .argv .slice (2));
+
+   systemSync ("npm", "run", "convert", "--", ... argv);
+   systemSync ("npm", "run", "image",   "--", ... argv);
+   systemSync ("npm", "run", "zip",     "--", ... argv);
+}
+
+main ();
+
