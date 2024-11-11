0a5241cd49a00c4a395dcd018ef7e5669f61bf71
Holger Seelig <holger.seelig@gmail.com>
2024-11-11 11:44:57 +0100

    Generate config.json.

--- build/config.js
+++ build/config.js
@@ -2,18 +2,38 @@
 "use strict";
 
 const
-   path = require ("path"),
-   fs   = require ( "fs")
+   path   = require ("path"),
+   fs     = require ( "fs"),
+   { sh } = require ("shell-tools")
 
 function main ()
 {
    console .log ("Updating docs/examples/config.json...");
 
    const
-      filePath = path .join (__dirname, "../docs/examples/config.json"),
+      examples = path .join (__dirname, "../docs/examples"),
+      filePath = path .join (examples, "config.json"),
       config   = require (filePath);
 
-   fs .writeFileSync (filePath, JSON .stringify (config, null, 2));
+   const tree = config .reduce ((p, c) => [p, Object .assign (p [c .component] ??= { }, { [c .name]: c })] [0], { });
+   const files = sh (`find '${examples}' -maxdepth 2 -mindepth 2 -type d`) .trim () .split ("\n") .sort ();
+
+   for (const folder of files)
+   {
+      const [component, name] = folder .split ("/") .splice (-2, 2);
+
+      tree [component]        ??= { };
+      tree [component] [name] ??= { };
+
+      const object = tree [component] [name];
+
+      object .name      = name;
+      object .component = component;
+   }
+
+   const modified = Object .values (tree) .flatMap (component => Object .values (component));
+
+   fs .writeFileSync (filePath, JSON .stringify (modified, null, 2));
 }
 
 main ();
