2a149ea985ed2cceedee0076ca4b48f6fe432184
Holger Seelig <holger.seelig@gmail.com>
2025-10-31 22:51:58 +0100

    Added x_ite-examples build script.

---
+++ build/x_ite-example.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+"use strict";
+
+const
+   path               = require ("path"),
+   yargs              = require ("yargs"),
+   { sh, systemSync } = require ("shell-tools");
+
+const
+   cwd      = process .cwd (),
+   examples = `${cwd}/docs/x_ite/examples`,
+   includes = new Set (process .argv .slice (2));
+
+const args = yargs (process .argv) .option ("delay",
+{
+   type: "number",
+   alias: "d",
+   requiresArg: true,
+   default: 0,
+})
+.argv;
+
+let extra = "";
+
+if (args .delay)
+   extra += `-d ${args .delay}`;
+
+function image (folder)
+{
+   const base = path .basename (folder);
+
+   if (includes .size && !includes .has (base))
+      return;
+
+   console .log (base);
+
+   process .chdir (folder);
+   systemSync (`npx --yes x3d-image ${extra} -s 480x270 -i ${base}.x3d -o screenshot.png`);
+}
+
+function main ()
+{
+   const files = sh (`find '${examples}' -maxdepth 1 -mindepth 1 -type d`) .trim () .split ("\n") .sort ();
+
+   for (const folder of files)
+      image (folder);
+}
+
+main ();
